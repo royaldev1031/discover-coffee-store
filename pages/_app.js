1db28cd416e8d5c8785fa493ed8d778d70be45e3
Daniel Morales <danieldev0612@gmail.com>
2022-06-29 06:52:19 -0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>@ 2022 Daniel</p>
+      </footer>
+    </div>
+  );
 }
 
 export default MyApp
