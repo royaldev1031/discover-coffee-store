cba62146fa7c8baf4649b6dbdbad314fd3c275e5
Daniel Morales <danieldev0612@gmail.com>
2022-07-01 19:34:17 -0700

    add card component, json coffee data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+
 import Banner from "../components/banner";
+import Card from "../components/card";
+
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
 
@@ -21,6 +25,20 @@ export default function Home() {
         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" alt="Hero Image" width={700} height={400} />
+        </div>
+        <div className={styles.cardLayout}>
+          { coffeeStores.map(coffeeStore => {
+              return (
+                <Card 
+                  key={coffeeStore.id}
+                  name={coffeeStore.name}
+                  imgUrl={coffeeStore.imgUrl}
+                  href={`/coffee-store/${coffeeStore.id}`}
+                  className={styles.card} 
+                />
+              );
+            })
+          }
         </div>
       </main>
     </div>
